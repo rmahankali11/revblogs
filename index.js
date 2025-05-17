baa4dc155febe4d8c307e5d4a1f3dc2e4fd64a78
rmahankali11 <156943071+rmahankali11@users.noreply.github.com>
2025-05-17 01:53:42 -0400

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import bodyParser from "body-parser";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 const __dirname = dirname(fileURLToPath(import.meta.url));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
